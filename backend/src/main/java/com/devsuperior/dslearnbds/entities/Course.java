07c3641b9b2fa270509fd2824e2e45fa650988a8
GiancarloJr <giancarlo456@hotmail.com>
2022-11-06 17:23:55 -0300

    Entity Course and SEED SQL

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Course.java
@@ -0,0 +1,76 @@
+package com.devsuperior.dslearnbds.entities;
+
+
+import javax.persistence.*;
+import java.util.Objects;
+
+
+@Entity
+@Table(name = "tb_course")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String imgUri;
+    @Column(columnDefinition = "TEXT")
+    private String imgGrayUri;
+
+    public Course(){
+        }
+
+    public Course(Long id, String name, String imgUri, String imgGrayUri) {
+        this.id = id;
+        this.name = name;
+        this.imgUri = imgUri;
+        this.imgGrayUri = imgGrayUri;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImgUri() {
+        return imgUri;
+    }
+
+    public void setImgUri(String imgUri) {
+        this.imgUri = imgUri;
+    }
+
+    public String getImgGrayUri() {
+        return imgGrayUri;
+    }
+
+    public void setImgGrayUri(String imgGrayUri) {
+        this.imgGrayUri = imgGrayUri;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return id.equals(course.id) && Objects.equals(name, course.name) && Objects.equals(imgUri, course.imgUri) && Objects.equals(imgGrayUri, course.imgGrayUri);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, imgUri, imgGrayUri);
+    }
+}
