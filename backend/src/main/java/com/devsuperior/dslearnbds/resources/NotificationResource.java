585962b48354848f7f5546e5699e27b159c2ed64
GiancarloJr <giancarlo456@hotmail.com>
2022-11-13 17:09:34 -0300

    Find Notification by User

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/resources/NotificationResource.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dslearnbds.resources;
+
+import com.devsuperior.dslearnbds.dto.NotificationDTO;
+import com.devsuperior.dslearnbds.services.NotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/notifications")
+public class NotificationResource {
+
+    @Autowired
+    private NotificationService NotificationService;
+
+    @GetMapping
+    public ResponseEntity<Page<NotificationDTO>> notificationForCurrentUser(Pageable pageable){
+        return ResponseEntity.ok().body(NotificationService.notificationForCurrentUser(pageable));
+    }
+
+}
