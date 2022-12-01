dcf4b115b5ef2856f975ba3213eaed9f700c5e0e
GiancarloJr <giancarlo456@hotmail.com>
2022-11-30 22:14:35 -0300

    Notifications Service,Resourse and Repository

--- backend/src/main/java/com/devsuperior/dslearnbds/resources/NotificationResource.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/resources/NotificationResource.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,8 +19,10 @@ public class NotificationResource {
     private NotificationService NotificationService;
 
     @GetMapping
-    public ResponseEntity<Page<NotificationDTO>> notificationForCurrentUser(Pageable pageable){
-        return ResponseEntity.ok().body(NotificationService.notificationForCurrentUser(pageable));
+    public ResponseEntity<Page<NotificationDTO>> notificationForCurrentUser(
+            @RequestParam(name="unreadOnly", defaultValue = "false") Boolean unreadOnly,
+            Pageable pageable){
+        return ResponseEntity.ok().body(NotificationService.notificationForCurrentUser(unreadOnly,pageable));
     }
 
 }
